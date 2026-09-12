630d18d50d88a85a3a2d6f20c6e3d91658cc9a63
Suman Pun Magar <mannmagar19@gmail.com>
2026-09-12 17:08:34 +0545

    Update TransactionContext.jsx for रू as default

--- src/contexts/TransactionContext.jsx
+++ src/contexts/TransactionContext.jsx
@@ -40,7 +40,7 @@ useEffect(() => {
 
   // Currency state
   const [currency, setCurrency] = useState(() => {
-    return localStorage.getItem("app_currency") || "Rs.";
+    return localStorage.getItem("app_currency") || "रू";
   });
 
   const updateCurrency = (symbol) => {
